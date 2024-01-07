cf1710d2650946d79fe5a9c070bdda5952d40b01
akshat <akshatbhatya@gmail.com>
2024-01-07 11:47:51 +0530

    add react router

--- shoping cart redux/src/App.jsx
+++ shoping cart redux/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+
 function App() {
   
 
   return (
     <>
+
+    <BrowserRouter>
+    <Routes>
+      <Route/>
+    </Routes>
+    </BrowserRouter>
+   
     </>
   )
 }
