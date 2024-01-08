c495d45bdee023df61e4e723618565a2487bd1bf
akshat <akshatbhatya@gmail.com>
2024-01-08 14:47:24 +0530

    add remove functionality

--- shoping cart redux/src/Pages/Cart.jsx
+++ shoping cart redux/src/Pages/Cart.jsx
@@ -1,22 +1,31 @@
-
-// import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { removeItem } from "../Components/Store/CartSlice"
 function Cart() {
 
-    // const cartItems=useSelector(state=>state.cart)
+    const dispatch = useDispatch()
+
+    const cartItems = useSelector(state => state.cart)
     return (
         <>
-            {/* {
-                cartItems.map((single)=>{
-                    return <>
-                    <img src={single.image} alt="" />
-                    <h2>{single.title}</h2>
-                    
-                    <h5>{single.description}</h5>
-                    <h3>{single.price}</h3>
-                    </>
-                })
-            } */}
-            
+
+            <div className="cards_parent">
+
+                {
+                    cartItems.map((single) => {
+
+                        return <div key={single.id} className="card">
+                            <img src={single.image} width={120} alt="" />
+                            <h4>{single.category}</h4>
+                            <h4>: {single.description} </h4>
+                            <h3>Price : {single.price} </h3>
+
+                            <button onClick={()=>dispatch(removeItem({id:single.id}))} >Remove</button>
+                        </div>
+
+                    })
+                }
+            </div>
+
 
         </>
     )
